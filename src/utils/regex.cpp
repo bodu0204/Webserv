a103396ccce618c5da386895bd48e18f4d0ac49f
blyu <blyu@xiaowendeiMac.local>
2023-05-30 17:55:42 +0900

    ...

--- src/utils/regex.cpp
+++ src/utils/regex.cpp
@@ -1,5 +1,6 @@
 #include "utils.hpp"
 #include <vector>
+#include <stack>
 
 static bool set_areas(std::vector<utils::meta::area>&, utils::meta::area);
 size_t _regex(const char *, const utils::meta::area &, size_t);
@@ -32,8 +33,7 @@ size_t _regex_std(const char *target, const utils::meta::area &search, size_t mo
 		else if (search.start[sl] == '['){
 			if(!utils::meta::_bracket(search.start + sl, "[]", '\\',(a.len)) || a.len < 3)
 				return (0);
-		}
-		else if (search.start[sl] == '('){
+		}else if (search.start[sl] == '('){
 			if(!utils::meta::_bracket(search.start + sl, "()[]", '\\',(a.len)) || a.len < 3)
 				return (0);
 		}
@@ -44,10 +44,50 @@ size_t _regex_std(const char *target, const utils::meta::area &search, size_t mo
 		if (!_regex_char(target[ret], a))
 			return (0);
 	}
+	utils::meta::area b;
+	b.start = search.start + sl + a.len;
+	b.len = search.len - sl - a.len;
 	if (search.start[sl] == '('){
-		
-	}
-	else if (memchr("?*+", search.start[sl + a.len], 3))
+		std::stack<size_t>s;
+		size_t l;
+		char c = search.start[sl + a.len];
+		switch (c)
+		{
+		case '*':
+		case '?':
+			if (!b.len)
+				return (ret);
+			l = _regex_std(target + ret, b, more > ret ? more - ret : 0);
+			if (l)
+				return (ret + l);
+		case '+':
+		default :
+			a.len -= 2;
+			a.start++;
+			s.push(0);
+			size_t lb = 0;
+			do{
+				s.top() = _regex_pipe(target + ret + lb, a, s.top() + 1);
+				if (s.top()){
+					if (!b.len)
+						return (ret + lb + s.top());
+					l = _regex_std(target + ret + lb + s.top(), b, more > ret + lb + s.top() ? more - ret - lb - s.top() : 0);
+					if (l)
+						return (ret + lb + s.top() + l);
+				}
+				if (s.top() && memchr("*+", c, 2)){
+					lb += s.top();
+					s.push(0);
+				}
+				else if (!s.top()){
+					s.pop();
+					if (!s.size())
+						return (0);
+					lb -= s.top();
+				}
+			} while (true);
+		}
+	}else if (memchr("?*+", search.start[sl + a.len], 3)){}
 	if (sl >= search.len)
 			return (ret >= more ? ret : 0);
 	else if (!target[ret])
